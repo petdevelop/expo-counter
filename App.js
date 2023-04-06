f3bb4068c34e850b7e9977197b67d5c2630fc804
Pedro Arronte <pedroarronte@Pedros-MBP.lan>
2023-04-06 14:50:50 -0400

    added redux

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import Counter from './pages/Counter'
+
+import { store } from './redux/store'
+import { Provider } from 'react-redux'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <Counter />
+    </Provider>
   );
 }
 
